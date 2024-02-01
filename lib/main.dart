cfd6de4ae6aa605dee30f951b3b8428bcdf0f724
Senali-perera <senaliperera1996@gmail.com>
2024-02-01 16:36:04 +0200

    Added the animated splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:lottie/lottie.dart';
 import 'package:mobile_computing_project/shopping/view_shopping_list.dart';
+import 'package:page_transition/page_transition.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 void main()  {
@@ -24,12 +27,19 @@ class MyApp extends StatelessWidget {
     requestPermission();
 
     return MaterialApp(
-      theme: ThemeData(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
         colorSchemeSeed: const Color(0xff6750a4),
         useMaterial3: true,
       ),
       title: title,
-      home: ViewShoppingList(),
+      home: AnimatedSplashScreen(
+          duration: 3000,
+          splash: Lottie.asset('assets/json/shopping_animated.json'),
+          nextScreen: ViewShoppingList(),
+          splashTransition: SplashTransition.fadeTransition,
+          pageTransitionType: PageTransitionType.rightToLeft,
+          backgroundColor: Colors.blue)
     );
   }
 }
